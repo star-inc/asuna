a0f6564d8ae1c8dc917d45b840a9dda41121022c
不知火 Shiranui <supersonic@livemail.tw>
2025-08-14 14:23:58 +0800

    feat: implement asuna prototype

--- src/init/instance.ts
+++ src/init/instance.ts
@@ -1,4 +1,4 @@
-// Lavateinn - Tiny and flexible microservice framework.
+// Asuna - Tiny and flexible microservice framework.
 // SPDX-License-Identifier: BSD-3-Clause (https://ncurl.xyz/s/mI23sevHR)
 
 // define application instance variables here.
