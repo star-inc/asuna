ff858ec9e6d8cece73e7df83ce7473743a8eab83
不知火 Shiranui <supersonic@livemail.tw>
2025-12-06 16:11:49 +0800

    fix(queue): improve message handling in receive method

--- src/init/queue.ts
+++ src/init/queue.ts
@@ -18,14 +18,9 @@ const amqpDurable = getEnabled('AMPQ_DURABLE');
 
 /**
  * @param message - The message.
- */
-type SubscribeCallback = (message: amqp.ConsumeMessage|null) => void;
-
-/**
  * @param channel - The channel instance.
- * @param message - The message.
  */
-type ReceiveCallback = (channel: amqp.Channel, message: amqp.ConsumeMessage|null) => void;
+type PayloadCallback = (message: amqp.ConsumeMessage|null, channel: amqp.Channel) => void;
 
 /**
  * Asuna Queue.
@@ -73,10 +68,12 @@ class Queue {
    * @param topic - The topic to subscribe.
    * @param callback - The callback function.
    */
-  subscribe(topic: string, callback: SubscribeCallback): void {
+  subscribe(topic: string, callback: PayloadCallback): void {
     if (!this._channel) throw new Error('Channel is not initialized');
     this._channel.assertQueue(topic, { durable: amqpDurable });
-    this._channel.consume(topic, callback, { noAck: true });
+    this._channel.consume(topic, (message) => {
+      callback(message, this._channel!);
+    }, { noAck: true });
   }
 
   /**
@@ -84,11 +81,11 @@ class Queue {
    * @param topic - The topic to receive.
    * @param callback - The callback function.
    */
-  receive(topic: string, callback: ReceiveCallback): void {
+  receive(topic: string, callback: PayloadCallback): void {
     if (!this._channel) throw new Error('Channel is not initialized');
     this._channel.assertQueue(topic, { durable: amqpDurable });
     this._channel.consume(topic, (message) => {
-      callback(this._channel!, message);
+      callback(message, this._channel!);
     }, { noAck: false });
   }
 
